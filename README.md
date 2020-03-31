43a20bb1f11f84cfd752d2d734a828d1837fec11
Rishichandra Wawhal <rishi@hasura.io>
2020-03-31 20:42:39 +0530

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 ```
 yarn
-yarn bundle
+```
+
+
+```
+# to cjs
+yarn cjs
+
+# to iife
+yarn iife
+
+# to umd
+yarn umd
 ```
---
+++ bundle.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const fetch = require('node-fetch');
+
+fetch('https://www.google.com').then(r => {
+  r.text().then(r => {
+    console.log(r);
+  });
+});
